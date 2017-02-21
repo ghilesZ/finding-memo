193ecd7fa45b457f26dfa51b5157d86ecf845231
ghiles <ghiles.ziat@gmail.com>
2017-02-21 09:53:32 +0100

    code cleaning in ppx

--- ppx_memo.ml
+++ ppx_memo.ml
@@ -14,41 +14,40 @@ let exp_is_rec = function
   | Pexp_let(Recursive,_,_) -> true
   | _ -> false
 
+let extend_mapper_with_expr mapper exprf = {default_mapper with expr = exprf}
+
 let extend_mapper argv =
-  { default_mapper with
-    expr = fun mapper expr ->
-           match expr with
-           (* let in *)
-           | { pexp_desc =
-                 Pexp_extension (
-                     {txt = "memo"},
-                     PStr [
-                         {pstr_desc =
-                            Pstr_eval (
-                                {pexp_loc; pexp_desc = Pexp_let(Recursive,value_binding, indulet)},
-                                _)
-                         }
-                   ])
-             }
-             -> error ~loc:pexp_loc "lol rec"
+  let exprf defexpr mapper expr =
+    match expr with
+    (* let in *)
+    | {pexp_desc =
+         Pexp_extension(
+             {txt = "memo"},
+             PStr [
+                 {pstr_desc =
+                    Pstr_eval (
+                        {pexp_loc; pexp_desc = Pexp_let(Recursive,_,_)},
+                        _)
+                 }
+           ])
+      } -> error ~loc:pexp_loc "let binding should not be recursive and memoized"
 
-           (* {e with pexp_desc = Pexp_let(r, handle value_binding, mapper.expr mapper indulet)} *)
-           | [%expr let%memo [%p? name] = fun [%p? param] -> [%e? corps] in [%e? indulet]] ->
-              let nem = check_nem name in
-              (* let variable = Pexp_ident (Longident.Lident nem) in *)
-              let variable = Exp.ident {txt=Longident.Lident nem; loc = name.ppat_loc} in
-              [%expr
-               let [%p name] =
-                 let [%p name] =
-                   fun [%p name] ->
-                   fun [%p param] ->
-                   [%e corps]
-                 in Memo.ize [%e variable]
-               in [%e mapper.expr mapper indulet]
-              ]
-           |  x -> default_mapper.expr mapper x
-  }
-
+    | [%expr let%memo [%p? name] = fun [%p? param] -> [%e? body] in [%e? scope]] ->
+       let nem = check_nem name in
+       let variable = Exp.ident {txt=Longident.Lident nem; loc = name.ppat_loc} in
+       [%expr
+        let [%p name] =
+          let open Memo in
+          let [%p name] =
+            fun [%p name] ->
+            fun [%p param] ->
+            [%e body]
+          in ize [%e variable]
+        in [%e mapper.expr mapper scope]
+       ]
+    |  x -> defexpr mapper x
+  in
+  extend_mapper_with_expr default_mapper (exprf default_mapper.expr)
 
 
 let () = register "memo" extend_mapper
