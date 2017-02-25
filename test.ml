107766f0b4a2d8a3d6a202009b88821e1ffd9d1f
ghiles <ghiles.ziat@gmail.com>
2017-02-25 16:39:35 +0100

    fix change-making problem

--- test.ml
+++ test.ml
@@ -1,25 +1,26 @@
-let l = [1;2;3]
-
+(* option utilities *)
 let optmin x y =
   match x,y with
-  | None,x | x,None -> x
-  | Some x, Some y -> Some (min x y)
+  | None,a | a,None -> a
+  | Some x, Some y-> Some (min x y)
 
 let succ = function
   | Some x -> Some (x+1)
-  | None -> Some 1
+  | None -> None
 
-(*Change-making problem*)
-let%memo change_make = function
-  | 0 -> Some 0
-  | n ->
-     let next acc x =
-       match n-x with
-       | 0 -> acc
-       | x when x < 0 -> None
-       | x -> change_make x |> succ |> optmin acc
-     in List.fold_left next None l
+(* Change-making problem*)
+let change_make coins value =
+  let%memo loop n =
+    let next acc piece =
+      match n-piece with
+      | 0 -> Some 1
+      | x -> if x < 0 then None
+             else optmin (succ (loop x)) acc
+    in
+    List.fold_left next None coins
+  in loop value
 
+(* testing *)
 let print fmt = function
   | None ->  Format.printf "no solution found"
   | Some x -> Format.printf "best solution : %i" x
@@ -32,5 +33,5 @@ let time f x =
   res
 
 let _ =
-  let x = 300 in
-  Format.printf "%a\n%!" print (time change_make x)
+  let x = 5101 in
+  Format.printf "%a\n%!" print (time (change_make [2;4;3;7;8]) x)
